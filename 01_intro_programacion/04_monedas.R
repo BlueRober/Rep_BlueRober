1c0318c08f6ed85a16cdec4c8a9319367cc0153c
BLUETAB-RSG\Bluetab <roberto.sequera@bluetab.net>
2017-12-05 00:57:01 +0100

    Corrección completa del ejercicio.

--- 01_intro_programacion/04_monedas.R
+++ 01_intro_programacion/04_monedas.R
@@ -4,160 +4,28 @@
 #¿De cuántas formas distintas podemos sumar £2, usando cualquier cantidad de monedas?
 
 
-#int target = 200;
-#int[] coinSizes = { 1, 2, 5, 10, 20, 50, 100, 200 };
-#int[] ways = new int[target+1];
-#ways[0] = 1;
+# Adaptado de http://www.mathblog.dk/project-euler-31-combinations-english-currency-denominations/
 
-#for (int i = 0; i < coinSizes.Length; i++) {
-#  for (int j = coinSizes[i]; j <= target; j++) {
-#    ways[j] += ways[j - coinSizes[i]];
-#  }
-#}
+# Variables iniciales
+objetivo <- 200
+monedas  <- c(1, 2, 5, 10, 20, 50, 100, 200)
 
+# Definimos el array de formas (alternativas para conseguir una cantidad)
+formas    <- rep(0, 201)
+formas[1] <- 1
 
-
-#int target  = 200;
-#int ways = 0;
-
-#for (int a = target; a >= 0; a -= 200) {
-#  for (int b = a; b >= 0; b -= 100) {
-#    for (int c = b; c >= 0; c -= 50) {
-#      for (int d = c; d >= 0; d -= 20) {
-#        for (int e = d; e >= 0; e -= 10) {
-#          for (int f = e; f >= 0; f -= 5) {
-#            for (int g = f; g >= 0; g -= 2) {
-#              ways++;
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#}
-
-
-
-rm(list = ls())
-target <- 200
-ways <- 0
-
-
-for ( a in target:target) {
-
-  if (a == 0){
-    next
+# Construimos el array de soluciones
+for (moneda in monedas) {  # Iteramos por moneda
+  for (i in (moneda + 1):(objetivo + 1)) {  # Iteramos hasta el objetivo
+    # para saber las formas de sumar x, consultamos formas[x + 1]
+    # es decir, para saber las formas de sumar 4 están en formas[5]
+    formas[i] = formas[i] + formas[i - moneda]
   }
-  else {
-    a = a - 200
-  }
-  
-  for ( b in a:target) {
-    
-    if (b == 0){
-      next
-    }
-    else {
-      b = b - 100
-    }
- 
-    for ( c in b:target) {
-      
-      if (c == 0){
-        next
-      }
-      else {
-        c = c - 50
-      }
-      
-      for ( d in c:target) {
-        
-        if (d == 0){
-          next
-        }
-        else {
-          d = d - 20
-        }
-        
-        for ( e in d:target) {
-
-          if (e == 0){
-            next
-          }
-          else {
-            e = e - 10
-          }
-          
-          for ( f in e:target) {
-
-            if (f == 0){
-              next
-            }
-            else {
-              f = f - 5
-            }
-            
-            for ( g in f:target) {
-
-              if (g == 0){
-                next
-              }
-              else {
-                g = g - 2
-              }
-              
-              for ( h in g:target) {
-
-                if (h == 0){
-                  next
-                }
-                else {
-                  h = h - 1
-                }
-                
-                
-                ways=ways+1
-                
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  print(formas)
 }
 
-
-8^8
-16.777.216
-
+# La solución está en la última posición del array
+formas[length(formas)]
 
 
-rm(list = ls())
-target <- 200
-ways <- 0
 
-for ( a in target:target) {
-  a = a - 200
-  x = a >= 0
-  ways=ways+1
-}
-
-
-target <- 201
-coinSizes <- c( 1, 2, 5, 10, 20, 50, 100, 200 )
-#ways <- target+1
-ways[0] = 1
-m_result <- matrix(0, nrow = 0, ncol = 0)
-m_result <- matrix(m_result,1)
-
-for (moneda in 1:length(coinSizes)){
-  
-  for (veces in coinSizes[moneda]:target ){
-    #m_result[veces] == m_result + m_result[veces - coinSizes[moneda]]
-    m_result[veces] =  (veces - coinSizes[moneda])
-    
-    
-  }
-  
-}
